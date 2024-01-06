7d56142cd1aa9362c3723221e73ed388f43abb37
AleksSultanov <91152765+AleksSultanov@users.noreply.github.com>
2024-01-06 12:16:31 +0500

    роутинг для гит

--- visomap-app/src/router/index.jsx
+++ visomap-app/src/router/index.jsx
@@ -1,30 +1,33 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Root } from '../pages/root.jsx';
-import { Places } from '../pages/places.jsx';
-import { Layout } from '../layout.jsx';
-import { ROUTE_PATHES } from './pathes.js';
-import { NotFoundPage } from '../pages/notFound.jsx';
+import { createBrowserRouter } from "react-router-dom";
+import { Root } from "../pages/root.jsx";
+import { Places } from "../pages/places.jsx";
+import { Layout } from "../layout.jsx";
+import { ROUTE_PATHES } from "./pathes.js";
+import { NotFoundPage } from "../pages/notFound.jsx";
 
-export const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: ROUTE_PATHES.root,
-        element: <Root />,
-      },
-      {
-        path: ROUTE_PATHES.base,
-        element: <Root />,
-      },
-      {
-        path: ROUTE_PATHES.places,
-        element: <Places />,
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: ROUTE_PATHES.root,
+          element: <Root />,
+        },
+        {
+          path: ROUTE_PATHES.base,
+          element: <Root />,
+        },
+        {
+          path: ROUTE_PATHES.places,
+          element: <Places />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/VisioMapGps/" }
+);
